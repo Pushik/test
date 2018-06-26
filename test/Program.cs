90e111e9ed09b13961630b1cfd28e84da4b866a1
Pushik <pushik1@yandex.ru>
2018-06-26 17:37:41 +0300

    почистил код

--- test/Program.cs
+++ test/Program.cs
@@ -25,10 +25,10 @@ namespace test
             // Чтение кода котировки
             Console.WriteLine("Enter Quote code:");
             code = Console.ReadLine();
-            Console.WriteLine("Enter Time Period:");
+            Console.WriteLine("Enter Time Period in second:");
             timertime = Convert.ToInt16(Console.ReadLine());
 
-            // Create a timer and set a two second interval.
+            // Создание таймера с принимаемым интервалом в переменной timertime
             aTimer = new System.Timers.Timer();
             aTimer.Interval = (timertime * 1000);
             aTimer.Elapsed += OnTimedEvent;  // Hook up the Elapsed event for the timer.
@@ -37,49 +37,13 @@ namespace test
 
             Console.WriteLine("Press any key to exit!");
             Console.ReadKey();
-
-
-            // Получение данных от сервера Yahoo
-
-            //  Console.WriteLine("Данные от сервера");
-            //  WebRequest wrGETURL = WebRequest.Create($"https://query1.finance.yahoo.com/v8/finance/chart/{code.ToUpperInvariant()}?interval=1d");
-            //  Stream objStream;
-            //  objStream = wrGETURL.GetResponse().GetResponseStream();
-            //   StreamReader objReader = new StreamReader(objStream);
-            //  var json = objReader.ReadToEnd();
-            //  var result = JsonConvert.DeserializeObject<Result>(json);
-
-            // Попытка собрать в массив
-
-            //decimal[] numbers = new decimal[5];
-            //for (int b = 0; b < numbers.Length; b++)
-            //    { numbers[b] = decimal.Parse(result.Chart.Data[0].Indicator.Quotes[0].Valueopen[0].ToString());
-            //    }
-
-            // Попытка вывести из массива
-
-            //Console.WriteLine("Массив значений OpeN");
-            //for (int b = 0; b < numbers.Length; b++)
-            // {
-            //    Console.WriteLine(numbers[b]);
-            // }
-            //Console.WriteLine("Press to exit");
-            //Console.ReadKey();
-
-            // вот тут выводится всё одной строкой
-
-            // console.writeline("raw sever reponse:");
-            // console.writeline(json);
-
-            var Res = new ResultYohoo();
+                       
                 
         }
 
         public static void OnTimedEvent(object source, System.Timers.ElapsedEventArgs e)
         {
             Console.WriteLine("the elapsed event was raised at {0}", e.SignalTime);
-           // Console.WriteLine(DataYohoo1(code));
-            
             var data = DataYohoo1(code);
             data.GetInfo();
             
@@ -90,6 +54,7 @@ namespace test
             // Получение данных от сервера Yahoo
             Console.WriteLine("Данные от сервера");
             WebRequest wrGETURL = WebRequest.Create($"https://query1.finance.yahoo.com/v8/finance/chart/{code.ToUpperInvariant()}?interval=1d");
+            // Пример получение данных по указанной котировке - MU
             // WebRequest wrGETURL = WebRequest.Create($"https://query1.finance.yahoo.com/v8/finance/chart/MU?interval=1d");
             Stream objStream;
             objStream = wrGETURL.GetResponse().GetResponseStream();
@@ -99,7 +64,7 @@ namespace test
 
             try
             {
-                // вот тут нужно запихнуть ответ от сервера и уже что-то будет ??? 
+                // присваиваем ответ от сервера 
                 var result = JsonConvert.DeserializeObject<Result>(json);
 
 
@@ -111,27 +76,12 @@ namespace test
                     Console.WriteLine("Bad object format");
                 }
                 else
-                {
+                {   // заполняем модель ResultYohoo 
                     var data1 = new ResultYohoo();
                     data1.Currency = result.Chart.Data[0].Metadata.Currency.ToString();
                     data1.ExchangeName = result.Chart.Data[0].Metadata.ExchangeName.ToString();
                     data1.Timezone = result.Chart.Data[0].Metadata.Timezone.ToString();
-                    
-                    
-                   
-                    // Console.WriteLine($"Current value for code: {result.Chart.Data[0].Indicator.CurrentValue[0].Value[0].ToString("#,#00.0000")}");
-                    // Console.WriteLine($"Current Curency : {result.Chart.Data[0].Metadata.Currency.ToString()}");
-                    // Console.WriteLine($"Exchange Name: {result.Chart.Data[0].Metadata.ExchangeName.ToString()}");
-                    // Console.WriteLine($"Exchange Name: {result.Chart.Data[0].Metadata.Timezone.ToString()}");
-                    // Console.WriteLine($"Exchange Time Zone :{result.Chart.Data[0].Metadata.ExchangeTimeZoneName.ToString()}");
-                    // Квота Открытие - закрытие
-                    //Console.WriteLine($"Value Open for code: {result.Chart.Data[0].Indicator.Quotes[0].Valueopen[0].ToString("#,#00.0000")}");
-                    //Console.WriteLine($"Value Close for code: {result.Chart.Data[0].Indicator.Quotes[0].Valueclose[0].ToString("#,#00.0000")}");
-                    // Разница между Open и Close  Quote 
-                    //decimal a = ((result.Chart.Data[0].Indicator.Quotes[0].Valueopen[0]) - (result.Chart.Data[0].Indicator.Quotes[0].Valueclose[0]));
-                    //Console.WriteLine($"Delta from Quote Open<->Close: {a}");
-                    //Console.WriteLine();
-                    // var b = result.Chart.Data[0].Indicator.Quotes[0].Valueopen[0];
+                    // возврат модели
                     return data1;
                     
                     Console.WriteLine("Press any key to exit!");
@@ -146,8 +96,8 @@ namespace test
                 return null;
             }
             finally
-            { aTimer.AutoReset = true;
-                
+            {
+                aTimer.AutoReset = true;
             }
 
             return null;
